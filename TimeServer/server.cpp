317328a7e522628e27f62462ecb1a1bc08c1ec9a
thamidurm <thamidurm@gmail.com>
2022-06-01 11:48:11 +0530

    add multi threading support to server

--- TimeServer/server.cpp
+++ TimeServer/server.cpp
@@ -2,13 +2,14 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+#include <chrono>
+#include <cstdio>
+#include <ctime>
+#include <thread>
 #include <Windows.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <iphlpapi.h>
-#include <cstdio>
-#include <chrono>
-#include <ctime>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -18,7 +19,7 @@ constexpr auto DEFAULT_BUFLEN = 512;
 // Server waits for a connection from a client
 // Once received, it checks the payload and if it is "Give_Time" returns the time
 // otherwise, returns "Unexpected Request"
-// Since multi-threading is used can only handle a single client
+
 int main()
 {
 	WSADATA wsaData;
@@ -73,6 +74,7 @@ int main()
 
 	freeaddrinfo(result);
 
+
 	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
 	{
 		printf("Listen failed with error: %ld\n", WSAGetLastError());
@@ -82,74 +84,71 @@ int main()
 	}
 
 	SOCKET ClientSocket = INVALID_SOCKET;
+	while (true) {
 
-	ClientSocket = accept(ListenSocket, nullptr, nullptr);
-	if (ClientSocket == INVALID_SOCKET)
-	{
-		printf("accept failed: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	char recvbuf[DEFAULT_BUFLEN];
-	int iSendResult;
-	int recvbuflen = DEFAULT_BUFLEN;
-
-	do
-	{
-		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-		if (iResult > 0)
-		{
-			char response[DEFAULT_BUFLEN];
-			if (strcmp(recvbuf, "GIVE_TIME"))
+		ClientSocket = accept(ListenSocket, nullptr, nullptr);
+		std::thread thread([ClientSocket]()
 			{
-				auto cur = std::chrono::system_clock::now();
-				std::time_t time = std::chrono::system_clock::to_time_t(cur);
-				strcpy(response, std::ctime(&time));
-			}
-			else
-			{
-				strcpy(response, "Unexpected request");
-			}
-			iSendResult = send(ClientSocket, response, (int)strlen(response) + 1, 0);
+				if (ClientSocket == INVALID_SOCKET)
+				{
+					printf("accept failed: %d\n", WSAGetLastError());
+					WSACleanup();
+				}
 
-			if (iSendResult == SOCKET_ERROR)
-			{
-				printf("send failed: %d\n", WSAGetLastError());
+				char recvbuf[DEFAULT_BUFLEN];
+				int iSendResult;
+				int recvbuflen = DEFAULT_BUFLEN;
+				int iResult;
+				do
+				{
+					iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+					if (iResult > 0)
+					{
+						char response[DEFAULT_BUFLEN];
+						if (strcmp(recvbuf, "GIVE_TIME"))
+						{
+							auto cur = std::chrono::system_clock::now();
+							std::time_t time = std::chrono::system_clock::to_time_t(cur);
+							strcpy(response, std::ctime(&time));
+						}
+						else
+						{
+							strcpy(response, "Unexpected request");
+						}
+						iSendResult = send(ClientSocket, response, (int)strlen(response) + 1, 0);
+
+						if (iSendResult == SOCKET_ERROR)
+						{
+							printf("send failed: %d\n", WSAGetLastError());
+							closesocket(ClientSocket);
+						}
+					}
+					else if (iResult == 0)
+					{
+						printf("Connection closing..\n");
+					}
+					else
+					{
+						printf("recv failed: %d\n", WSAGetLastError());
+						closesocket(ClientSocket);
+						
+					}
+				} while (iResult > 0);
+
+				iResult = shutdown(ClientSocket, SD_SEND);
+				if (iResult == SOCKET_ERROR)
+				{
+					printf("shutdown failed: %d\n", WSAGetLastError());
+					closesocket(ClientSocket);
+				}
+
 				closesocket(ClientSocket);
-				closesocket(ListenSocket);
-				WSACleanup();
-				return 1;
-			}
-			printf("Bytes sent: %d\n", iSendResult);
-		} 
-		else if (iResult == 0)
-		{
-			printf("Connection closing..\n");
-		}
-		else
-		{
-			printf("recv failed: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
-			closesocket(ListenSocket);
-			WSACleanup();
-			return 1;
-		}
-	} 
-	while (iResult > 0);
-	
-	iResult = shutdown(ClientSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR)
-	{
-		printf("shutdown failed: %d\n", WSAGetLastError());
-		closesocket(ClientSocket);
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
+			});
+		thread.detach();
 	}
 
-	closesocket(ClientSocket);
 	WSACleanup();
+
+	
 	return 0;
 }
